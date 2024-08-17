fa9e86f6d71ecfce9df939ebc29320b5ba30bbc9
x_Bastille <148121344+XBastille@users.noreply.github.com>
2024-08-17 18:14:52 +0530

    modification-5

--- model_2.py
+++ model_2.py
@@ -1,22 +1,26 @@
-# model_2.py
 import datetime
 import numpy as np
 import optuna
 import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+import requests
+import nltk
 import sys
-#import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from main import StockDataVisualizer
+from sklearn.metrics import mean_squared_error, r2_score
+from bs4 import BeautifulSoup
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import warnings
+
 warnings.filterwarnings("ignore")
 
 class StockPredictor:
-    def __init__(self, company_name, num_days_pred, stock_data):
+    def __init__(self, company_name, num_days_pred, no_of_trial, stock_data):
         self.company_name=company_name
         self.num_days_pred=num_days_pred
-        stock_data.drop(columns=["Open", "High", "Low", "Volume"], inplace=True)
+        self.no_of_trial=no_of_trial
         self.stock_data=stock_data
         self.df_xgb=self.prepare_data()
 
@@ -40,7 +44,32 @@ class StockPredictor:
         df["dayofyear"]=df.index.dayofyear
         df["dayofmonth"]=df.index.day
         df["weekofyear"]=df.index.isocalendar().week
+        ma_windows=[self.num_days_pred, 2*self.num_days_pred]
+        for window in ma_windows:
+            df[f"MA{window}"]=df["Close"].rolling(window=window).mean()
+        df["volatility"]=df["Close"].rolling(window=self.num_days_pred).std()
+        df["EMA"]=df["Close"].ewm(span=self.num_days_pred, adjust=False).mean()
+        df["momentum"]=df["Close"]-df["Close"].shift(self.num_days_pred//2)
+        df["sentiment"]=self.get_sentiment_score()
         return df
+
+    def get_sentiment_score(self):
+        headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        url=f"https://finance.yahoo.com/quote/{self.company_name}/news?p={self.company_name}"
+        r=requests.get(url, headers=headers)
+        soup=BeautifulSoup(r.text, "lxml")
+        headlines=[headline.text for headline in soup.find_all("h3", class_="clamp yf-1044anq")]
+        nltk.download('vader_lexicon')
+        sid=SentimentIntensityAnalyzer()
+        sentiment_scores=[]
+        ticker=yf.Ticker(self.company_name)
+        company_info=ticker.info
+        company_name=company_info.get('longName', 'Name not available')
+        for headline in headlines:
+            if self.company_name in headline or company_name[0].lower() in headline.lower():
+                sentiment=sid.polarity_scores(headline)
+                sentiment_scores.append(sentiment['compound'])
+        return np.mean(sentiment_scores) if sentiment_scores else 0
 
     def prepare_data(self):
         df=self.stock_data.copy()
@@ -50,9 +79,13 @@ class StockPredictor:
         return df
 
     def split_data(self):
-        X=self.df_xgb.drop(columns="Close")
-        y=self.df_xgb["Close"]
-        return train_test_split(X, y, test_size=0.2, random_state=42)
+        df_train=self.df_xgb.iloc[:-self.num_days_pred]
+        df_test=self.df_xgb.iloc[-self.num_days_pred:]
+        X_train=df_train.drop(columns="Close")
+        y_train=df_train["Close"]
+        X_test=df_test.drop(columns="Close")
+        y_test=df_test["Close"]
+        return X_train, X_test, y_train, y_test
 
     def optimize_model(self, X_train, y_train, X_test, y_test):
         def objective(trial):
@@ -72,15 +105,15 @@ class StockPredictor:
             model.fit(X_train, y_train)
             y_pred=model.predict(X_test)
             return np.sqrt(mean_squared_error(y_test, y_pred))
-        
+
         study=optuna.create_study(direction="minimize")
-        study.optimize(objective, n_trials=2)
+        study.optimize(objective, n_trials=self.no_of_trial)
         best_params=study.best_trial.params
         #print("Best parameters found by Optuna:")
         #for key, value in best_params.items():
         #    print(f"{key}: {value}")
         return best_params
-
+        
     def train_best_model(self, X_train, y_train, best_params):
         model=XGBRegressor(**best_params)
         model.fit(X_train, y_train)
@@ -132,18 +165,19 @@ class StockPredictor:
         future_predictions=self.predict_future(best_model)
         future_predictions.to_csv("stock_data.csv")
         percentage_change=self.calculate_percentage_change(future_predictions["pred"])
-        '''print(f"Predicted Future Prices:\n{future_predictions}")
+        #print(f"Predicted Future Prices:\n{future_predictions}")
         print(f"Predicted percentage change over the next {self.num_days_pred} days: {percentage_change:.2f}%")
         if percentage_change>0:
             print("The model predicts an upward trend. It might be a good time to buy.")
         else:
-            print("The model predicts a downward trend. It might be better to wait.")'''
+            print("The model predicts a downward trend. It might be better to wait.")
 
 if __name__=="__main__":
     if len(sys.argv)<3:
         sys.exit(1)
     company_name=sys.argv[1]
     num_days_pred=int(sys.argv[2])
+    no_of_trial=int(sys.argv[3])
     data=StockDataVisualizer(company_name)
-    predictor=StockPredictor(company_name, num_days_pred, data.download_stock_data())
+    predictor=StockPredictor(company_name, num_days_pred, no_of_trial, data.stock_data)
     predictor.run()
