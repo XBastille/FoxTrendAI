816bf2a55d101b2cadea48084bf710dfa6d87b26
x_Bastille <148121344+XBastille@users.noreply.github.com>
2024-08-22 21:16:59 +0530

    made the dynamic

--- graphinput.js
+++ graphinput.js
@@ -1,19 +1,21 @@
 const express=require('express');
-const {spawn}=require('child_process')
+const { spawn }=require('child_process');
 const app=express();
 const port=3000;
-app.get('/trial/:num1/:num2/:num3',(req,res)=>{
+
+app.get('/trial/:numCompanies/:tickers/:startDate/:endDate', (req, res) => {
     let data1='';
-    const pyone=spawn('python',['main.py',req.params.num1,req.params.num2,req.params.num3])
-    pyone.stdout.on('data',function(data){
+    const tickers=req.params.tickers.split(','); 
+    const args=[req.params.numCompanies, ...tickers, req.params.startDate, req.params.endDate];
+    const pyone=spawn('python', ['trial.py', ...args]);
+    pyone.stdout.on('data', function (data) {
         data1+=data.toString();
-    })
-    pyone.on('close',(code)=>{
+    });
+    pyone.on('close', (code)=>{
         console.log(`child process close all stdio with code ${code}`);
         res.send(data1);
     });
 });
-
-app.listen(port,()=>{
-    console.log(`app is listening to the ${port} `)
-})
+app.listen(port, ()=>{
+    console.log(`app is listening to the ${port}`);
+});
