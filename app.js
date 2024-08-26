08ea3c302da9bdcbc1805f8ee218a9a32cc14412
ABHINAB SHARMA <146541054+Abhinab04@users.noreply.github.com>
2024-08-26 15:25:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+const port=3000;
+
+
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use("/",require("./route/graph"))
+
+app.listen(port, ()=>{
+    console.log(`app is listening to the ${port}`);
+});
---
+++ route/graph.js
@@ -0,0 +1,37 @@
+
+const express=require('express');
+const router = express.Router();
+const { spawn }=require('child_process');
+
+
+
+router.get("/",(req,res)=>{
+    res.render("template")
+})
+ 
+
+
+router.post('/trials', (req, res) => {
+    let data1='';
+    
+    const numCompanies=req.body.numCompanies;
+    const startDate=req.body.startDate;
+    const endDate=req.body.endDate;
+    const tickers=req.body.tickers.split(','); 
+    const args=[numCompanies, ...tickers, startDate, endDate];
+
+    console.log("Arguments :"+args)
+
+    const pyone=spawn('python', ['main.py', ...args]);
+    pyone.stdout.on('data', function (data) {
+        data1+=data.toString();
+    });
+
+    pyone.on('close', (code)=>{
+        console.log(`child process close all stdio with code ${code}`);
+        res.send(data1);
+    });
+});
+
+
+module.exports=router;
