d06582207be31f7d60e85b978a29a8e5e7b36437
ABHINAB SHARMA <146541054+Abhinab04@users.noreply.github.com>
2024-08-26 15:24:25 +0530

    Update graphtrail.js

--- graphinput.js
+++ graphinput.js
@@ -1,21 +1,37 @@
-const express=require('express');
-const { spawn }=require('child_process');
-const app=express();
-const port=3000;
 
-app.get('/trial/:numCompanies/:tickers/:startDate/:endDate', (req, res) => {
+const express=require('express');
+const router = express.Router();
+const { spawn }=require('child_process');
+
+
+
+router.get("/",(req,res)=>{
+    res.render("template")
+})
+ 
+
+
+router.post('/trials', (req, res) => {
     let data1='';
-    const tickers=req.params.tickers.split(','); 
-    const args=[req.params.numCompanies, ...tickers, req.params.startDate, req.params.endDate];
-    const pyone=spawn('python', ['trial.py', ...args]);
+    
+    const numCompanies=req.body.numCompanies;
+    const startDate=req.body.startDate;
+    const endDate=req.body.endDate;
+    const tickers=req.body.tickers.split(','); 
+    const args=[numCompanies, ...tickers, startDate, endDate];
+
+    console.log("Arguments :"+args)
+
+    const pyone=spawn('python', ['main.py', ...args]);
     pyone.stdout.on('data', function (data) {
         data1+=data.toString();
     });
+
     pyone.on('close', (code)=>{
         console.log(`child process close all stdio with code ${code}`);
         res.send(data1);
     });
 });
-app.listen(port, ()=>{
-    console.log(`app is listening to the ${port}`);
-});
+
+
+module.exports=router;
