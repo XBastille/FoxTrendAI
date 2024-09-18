86c0b2e2288c2f47c85710de1b440a474f02141d
Bibhor Puhan <148121344+XBastille@users.noreply.github.com>
2024-09-18 12:53:47 +0530

    Update Stockspage.js

--- Stockspage.js
+++ Stockspage.js
@@ -1,14 +1,21 @@
-document.querySelector(".Arrow").addEventListener('Click' , function(){
+document.querySelector(".Arrow").addEventListener('click' , function(){
     document.querySelectorAll(".Icon-items").forEach(function(item){
           const iconName = item.querySelector('.icon-name');
           const nameText = item.getAttribute('data-name');
 
-          if(iconName.style.display === "none" || iconName.style.display === ' ') {
+          if(iconName.style.display === "none" || iconName.style.display === '') {
             iconName.textContent = nameText;
             iconName.style.display = 'inline-block';
+            iconName.style.display = 'flex';
           }else{
             iconName.style.display = 'none';
           }
     })
-    
 })
+    document.querySelector(".cha").addEventListener('click' , function(){
+        const SUMMARY = document.querySelector('.sum');
+        var CHART = document.querySelector('.cha');
+        SUMMARY.style.backgroundColor= "black";
+        CHART.style.backgroundColor = "purple";
+    })
+    
