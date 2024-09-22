69f40b8ab0410cec8c13dfd1c78e49bcde8291f8
Ayush10761op <163870658+Ayush10761op@users.noreply.github.com>
2024-09-22 23:35:48 +0530

    Add files via upload

--- Stockspage.js
+++ Stockspage.js
@@ -9,22 +9,32 @@ document.querySelector(".Arrow").addEventListener('click' , function(){
           }else{
             iconName.style.display = 'none';
           }
-          
-          
+             
           
     })
+
   }) 
+
+document.querySelector(".Arrow2").addEventListener('click' , function(){
+   const ARROW = document.querySelector('.Arrow');
+   const iconName = item.querySelector('.icon-name');
+   const nameText = item.getAttribute('data-name');
+
+   ARROW.style.display = "none";
+
+})  
   
   document.querySelector(".sum").addEventListener('click' , function(){
     const SUMMARY = document.querySelector('.sum');
     const CHART = document.querySelector('.cha');
     const HISTO = document.querySelector('.histo');
+    const STAT = document.querySelector('.stat');
     const PRO = document.querySelector('.pro');
     SUMMARY.style.backgroundColor= " rgb(96, 0, 147)";
     HISTO.style.backgroundColor = "black";
     PRO.style.backgroundColor = "black";
     CHART.style.backgroundColor = "black";
-    SUMMARY.borderColor = " rgb(96, 0, 147) ";
+    SUMMARY.border = "10px solid rgb(96, 0, 147)";
     
 })
 
@@ -32,20 +42,37 @@ document.querySelector(".Arrow").addEventListener('click' , function(){
     const SUMMARY = document.querySelector('.sum');
     const CHART = document.querySelector('.cha');
     const HISTO = document.querySelector('.histo');
+    const STAT = document.querySelector('.stat');
     const PRO = document.querySelector('.pro');
     SUMMARY.style.backgroundColor= "black";
     HISTO.style.backgroundColor = "black";
+    STAT.style.backgroundColor = "black";
     PRO.style.backgroundColor = "black";
-    CHART.style.backgroundColor = "purple";
+    CHART.style.backgroundColor = "rgb(96, 0, 147)";
 })
 
 document.querySelector(".histo").addEventListener('click' , function(){
   const SUMMARY = document.querySelector('.sum');
   const CHART = document.querySelector('.cha');
   const HISTO = document.querySelector('.histo');
+  const STAT = document.querySelector('.stat');
   const PRO = document.querySelector('.pro');
   SUMMARY.style.backgroundColor= "black";
-  HISTO.style.backgroundColor = "purple";
+  HISTO.style.backgroundColor = "rgb(96, 0, 147)";
+  STAT.style.backgroundColor = "black";
+  PRO.style.backgroundColor = "black";
+  CHART.style.backgroundColor = "black";
+})
+
+document.querySelector(".stat").addEventListener('click' , function(){
+  const SUMMARY = document.querySelector('.sum');
+  const CHART = document.querySelector('.cha');
+  const HISTO = document.querySelector('.histo');
+  const STAT = document.querySelector('.stat');
+  const PRO = document.querySelector('.pro');
+  SUMMARY.style.backgroundColor= "black";
+  HISTO.style.backgroundColor = "black";
+  STAT.style.backgroundColor = "rgb(96, 0, 147)";
   PRO.style.backgroundColor = "black";
   CHART.style.backgroundColor = "black";
 })
@@ -54,10 +81,12 @@ document.querySelector(".pro").addEventListener('click' , function(){
   const SUMMARY = document.querySelector('.sum');
   const CHART = document.querySelector('.cha');
   const HISTO = document.querySelector('.histo');
+  const STAT = document.querySelector('.stat');
   const PRO = document.querySelector('.pro');
   SUMMARY.style.backgroundColor= "black";
   HISTO.style.backgroundColor = "black";
-  PRO.style.backgroundColor = "purple";
+  STAT.style.backgroundColor = "black";
+  PRO.style.backgroundColor = "rgb(96, 0, 147)";
   CHART.style.backgroundColor = "black";
 })
 
@@ -86,7 +115,7 @@ function changeReadMore(){
 
     let currentSlide = 0;
     const slideWidth = 210; 
-    const visibleSlides = 7;
+    const visibleSlides = 5;
     const totalSlides = document.querySelectorAll('.company-box').length;
     const slider = document.getElementById('slider');
 
