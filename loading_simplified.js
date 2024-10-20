b1aae7fc36f8d834cfd9e753133124f4d49ab957
Bibhor Puhan <148121344+XBastille@users.noreply.github.com>
2024-10-20 14:05:14 +0530

    added simplified ver. of loading animation

---
+++ loading_simplified.js
@@ -0,0 +1,12 @@
+function restartCubeAnimation() {
+    const cube = document.querySelector('.cube');
+    const cubeEdges = cube.querySelectorAll('.cube-edge');
+    cubeEdges.forEach(edge => {
+        const clone = edge.cloneNode(true);
+        edge.parentNode.replaceChild(clone, edge);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setInterval(restartCubeAnimation, 11600);
+});
